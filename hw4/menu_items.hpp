d21d39c9e1c8e0075644a481b0cb5b655b4637e6
Polina Stoliarova <polinastoliarova@MacBook-Air-Polina.local>
2024-12-13 17:10:11 +0300

    a

---
+++ hw4/menu_items.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "menu.hpp"
+
+namespace polinom {
+    extern const MenuItem MAIN;
+
+    extern const MenuItem STUDY;
+    extern const MenuItem EXIT;
+
+    extern const MenuItem STUDY_SUMM;
+    extern const MenuItem STUDY_SUBSTRACT;
+    extern const MenuItem STUDY_MULTIPLY;
+    extern const MenuItem STUDY_DIVIDE;
+    extern const MenuItem STUDY_GO_BACK;
+
+    extern const MenuItem STUDY_BACK;
+    extern const MenuItem STUDY_DUB;
+    extern const MenuItem STUDY_ONEGIN;
+    extern const MenuItem STUDY_EVENING;
+}
